40f8774674cffde4fc3d2c0ff9ed6bb0b6247fd0
Jose Rodriguez <nalua72@gmail.com>
2026-04-30 13:32:38 +0200

    fix: update conftest.py so tests use in-memory DB

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,16 +5,19 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 from app.db.base import Base  # here residdes declarative_base()
+from app.db.sessions import get_db
 
 
 # 1. Creates a test engine database in memory
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+                       'check_same_thread': False}, poolclass=StaticPool)
 
 
 # 2.  SessionLocal for testing
@@ -38,6 +41,17 @@ def session():
         Base.metadata.drop_all(bind=engine)
 
 
+def get_db_override() -> Session:
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @pytest.fixture
-def client():
+def client(session):
+
+    app.dependency_overrides[get_db] = get_db_override
+
     return TestClient(app)
